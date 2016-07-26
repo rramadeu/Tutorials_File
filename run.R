ef8ac82203fe9c1fec3e71ab39755a530c24179a
Rodrigo Rampazo Amadeu <rramadeu@gmail.com>
2016-07-26 11:42:36 -0300

    Update run.R

--- run.R
+++ run.R
@@ -14,3 +14,18 @@ biocLite("goseq")
 biocLite("VariantAnnotation")
 biocLite("qvalues")
 
+
+require("adegenet")
+require("poppr")
+require("hierfstat")
+require("diveRsity")
+require("vegan")
+require("ape")
+require("ade4")
+require("pegas")
+require("ggplot2")
+require("locfit")
+require("edgeR")
+require("goseq") #erro de dependencia
+require("VariantAnnotation")
+require("qvalue")
